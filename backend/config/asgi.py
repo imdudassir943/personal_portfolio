ab127b3648c64fff94e6c7d5c7960c24ed8b9f8a
Mudassir Sharif <muhammadmudassirpk943@gmail.com>
2026-03-08 19:23:52 +0500

    resolve db issues

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -6,6 +6,6 @@ Exposes the ASGI callable as a module-level variable named ``application``.
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_asgi_application()
