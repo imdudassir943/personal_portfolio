2e4c860e0a9fc210af3f52470f5f5024efa74649
Mudassir Sharif <muhammadmudassirpk943@gmail.com>
2026-03-08 19:50:41 +0500

    fix: use CompressedStaticFilesStorage to avoid missing .map file errors

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
+export DJANGO_SETTINGS_MODULE=config.settings.production
 python manage.py collectstatic --noinput
 python manage.py migrate
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -40,7 +40,7 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 CLOUDINARY_STORAGE = {
